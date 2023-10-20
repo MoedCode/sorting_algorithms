6a09889f05acac361d5f713c3c5eb7aa46be0679
MoedCode <mohamed71291@gmail.com>
2023-10-21 00:26:27 +0300

    Sorting Algorithms (automated command)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,11 +6,9 @@
  * @size: size of the array
  * Return: (void)
 */
-
 void bubble_sort(int *array, size_t size)
 {
-	 /* Set initially to 1 to ensure at least one pass */
-	int is_swp = 1, tmp;
+	int is_swp = 1;
 	size_t i;
 
 	if (!array || !size || size < 2)
@@ -18,17 +16,19 @@ void bubble_sort(int *array, size_t size)
 
 	while (is_swp)
 	{
-		is_swp = 0;  /* Reset the flag at the beginning of each pass */
+		is_swp = 0;
 		for (i = 0; i < size - 1; ++i)
 		{
 			if (array[i] > array[i + 1])
 			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				is_swp = 1;  /* Set the flag if a swap occurred */
-				print_array(array, size);
+				array[i] ^= array[i + 1];
+				array[i + 1] ^= array[i];
+				array[i] ^= array[i + 1];
+				is_swp = 1;
 			}
 		}
+		if (is_swp)
+			print_array(array, size);
 	}
 }
+
