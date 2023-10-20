984dcdfc988bc49b9de01a814caa9f80ff00e1e7
MoedCode <mohamed71291@gmail.com>
2023-10-21 00:56:59 +0300

    Sorting Algorithms (automated command)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,28 @@
 #include "sort.h"
+/**
+ * swap - swaps  tow integers
+ * @x: pointer to  first integers
+ * @y: pointer to  second integers
+ * Return: void
+*/
+void swap(int *x, int *y)
+{
+	int tmp;
+
+	if (x == NULL || y == NULL)
+	{
+		/*
+		x == NULL && fprintf(stderr, "X is nill ");
+		y == NULL && fprintf(stderr, "y is nill ");
+		return;
+		*/
+		return;
+	}
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+
+}
 
 /**
  * bubble_sort - sorts a given array of integers  using bubble sort algorithm
@@ -13,6 +37,8 @@ void bubble_sort(int *array, size_t size)
 	int is_swp = 1;
 	size_t i;
 
+	if (!array || !size || size < 2)
+		return;
 
 	while (is_swp)
 	{
@@ -21,9 +47,12 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
+				/*
 				array[i] ^= array[i + 1];
 				array[i + 1] ^= array[i];
 				array[i] ^= array[i + 1];
+				*/
+				swap(&array[i], &array[i + 1]);
 				is_swp = 1;
 				print_array(array, size);
 			}
--- sort.h
+++ sort.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 
 int printIntAr(int arr[], size_t size);
 void swap(int *x, int *y);
+
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 #endif
