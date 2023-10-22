bdcee286d3ce5c87a437161070eaf590bc4fea7f
MoedCode <mohamed71291@gmail.com>
2023-10-22 04:06:17 +0300

    Commit message

--- sort.h
+++ sort.h
@@ -23,9 +23,11 @@ int printIntAr(int arr[], size_t size);
 void swap(int *x, int *y);
 
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
+void insertion_sort_list_(listint_t **list);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void selection_sort_1(int *array, size_t size);
+
 
 #endif
