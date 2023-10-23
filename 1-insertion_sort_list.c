9cd43a48c5aa6a28d280d83ed816b3ee3f821372
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-24 02:45:48 +0300

    first commit (A.C)

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -17,9 +17,6 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-
 	curr = *list;
 
 	while (curr != NULL)
