dfb24b3d6ef3576cec8fab85ac173f18cff3aaa9
MoedCode <mohamed71291@gmail.com>
2023-10-23 03:32:09 +0300

    Sorting Algorithms (automated command)

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * swap - swaps  tow integers
- * @x: pointer to  first integers
- * @y: pointer to  second integers
+ * swap - swaps two integers
+ * @x: pointer to the first integer
+ * @y: pointer to the second integer
  * Return: void
  */
 void swap(int *x, int *y)
@@ -19,16 +20,16 @@ void swap(int *x, int *y)
 	tmp = *x;
 	*x = *y;
 	*y = tmp;
-
 }
+
 /**
- * lo_partition - sort the array portion according to start and end
+ * lo_partition - sorts the array portion according to start and end
  *
  * @array: pointer to array
- * @size: sizeof(array)
+ * @size: size of the array
  * @start_idx: index to start from
  * @end_idx: index to end with
- * @Return: (int) index of the pivot, (0) on error
+ * Return: index of the pivot, 0 on error
  */
 int lo_partition(int *array, size_t size, int start_idx, int end_idx)
 {
@@ -54,43 +55,44 @@ int lo_partition(int *array, size_t size, int start_idx, int end_idx)
 	}
 	return (i);
 }
+
 /**
- * str_end_sort-   recursive function that sort  array  using quicksort algorithm .
- * @array: pointer to  array of integers to sort.
- * @start: index to start array partition to order  with  .
- * @end_idx: index to end array partition to order  with .
- * Return: (1) on error if array is NULL or start !< end else return (0)
-  */
+ * str_end_sort - recursive function that sorts the array using
+ * quick sort algorithm
+ * @array: pointer to the array of integers to sort
+ * @size: size of the array
+ * @start: index to start array partition to order
+ * @end_idx: index to end array partition to order
+ * Return: 1 on error if array is NULL or start <= end, 0 otherwise
+ */
 int str_end_sort(int array[], int size, int start, int end)
 {
 	int pivot;
 
 	srand(time(NULL));
 
-	if(array && start <= end)
+	if (array && start < end)
 	{
-		pivot = lo_partition(array, size, start,end);
+		pivot = lo_partition(array, size, start, end);
 
 		str_end_sort(array, size, start, pivot - 1);
 		str_end_sort(array, size, pivot + 1, end);
-
 	}
 	else
-		return (-1);
+		return (1);
+
 	return (0);
 }
+
 /**
- * quick_sort -  sorting array using recursive  str_end_sort function
- * @array: pointer to array
+ * quick_sort - sorting array using recursive str_end_sort function
+ * @array: pointer to the array
  * @size: size of the array
  */
-
 void quick_sort(int *array, size_t size)
 {
-	 if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-	str_end_sort(array, size, 0, size -1);
-
+	str_end_sort(array, size, 0, size - 1);
 }
-
