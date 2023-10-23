823d3cec1f772d62225292c0a9ce920be7169dde
MoedCode <mohamed71291@gmail.com>
2023-10-23 03:33:54 +0300

    Sorting Algorithms (automated command)

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,42 +1,36 @@
 #include "sort.h"
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * swap - swaps two integers
- * @x: pointer to the first integer
- * @y: pointer to the second integer
- * Return: void
+ * swap - swaps two elements in ascending order.
+ * @x: a pointer to an integer.
+ * @y: a pointer to an integer.
  */
 void swap(int *x, int *y)
 {
-	int tmp;
+	int temp;
 
-	if (x == NULL || y == NULL)
-		return;
-
-	tmp = *x;
+	temp = *x;
 	*x = *y;
-	*y = tmp;
+	*y = temp;
 }
-
 /**
- * lo_partition - sorts the array portion according to start and end
+ * lomuto_partition - Lomuto partition.
+ * @array: The array to be sorted.
+ * @size: the size of the array.
+ * @low: starting index.
+ * @high: the ending index.
+ * Return: the index of the new pivot.
  *
- * @array: pointer to array
- * @size: size of the array
- * @start_idx: index to start from
- * @end_idx: index to end with
- * Return: index of the pivot, 0 on error
+ * Description: The Lomuto partition scheme is
+ *		a partitioning scheme used in the QuickSort
+ *		algorithm to divide an array into two sub-arrays.
  */
-int lo_partition(int *array, size_t size, int start_idx, int end_idx)
+int lomuto_partition(int *array, size_t size, int low, int high)
 {
-	int pivot = array[end_idx];
-	int j, i = start_idx;
+	int pivot = array[high];
+	int j, i = low;
 
-	for (j = start_idx; j < end_idx; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -50,49 +44,39 @@ int lo_partition(int *array, size_t size, int start_idx, int end_idx)
 	}
 	if (array[i] > pivot)
 	{
-		swap(&array[i], &array[end_idx]);
+		swap(&array[i], &array[high]);
 		print_array(array, size);
 	}
 	return (i);
 }
-
 /**
- * str_end_sort - recursive function that sorts the array using
- * quick sort algorithm
- * @array: pointer to the array of integers to sort
- * @size: size of the array
- * @start: index to start array partition to order
- * @end_idx: index to end array partition to order
- * Return: 1 on error if array is NULL or start <= end, 0 otherwise
+ * quick_sort_recursion - Aplys a recursive quick sorting.
+ * @array: the array to be sorted.
+ * @size: size of the array.
+ * @low: the starting index.
+ * @high: the ending index.
  */
-int str_end_sort(int array[], int size, int start, int end)
+void quick_sort_recursion(int *array, size_t size, int low, int high)
 {
 	int pivot;
 
-	srand(time(NULL));
-
-	if (array && start < end)
+	if (low < high)
 	{
-		pivot = lo_partition(array, size, start, end);
-
-		str_end_sort(array, size, start, pivot - 1);
-		str_end_sort(array, size, pivot + 1, end);
+		pivot = lomuto_partition(array, size, low, high);
+		quick_sort_recursion(array, size, low, pivot - 1);
+		quick_sort_recursion(array, size, pivot + 1, high);
 	}
-	else
-		return (1);
-
-	return (0);
 }
-
 /**
- * quick_sort - sorting array using recursive str_end_sort function
- * @array: pointer to the array
- * @size: size of the array
+ * quick_sort - sorts a an array of integers in ascending order.
+ * @array: the array to sort.
+ * @size: the size of the array.
+ *
+ * Description: This function uses the Quik sort algorithm.
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
-
-	str_end_sort(array, size, 0, size - 1);
+	quick_sort_recursion(array, size, 0, size - 1);
 }
