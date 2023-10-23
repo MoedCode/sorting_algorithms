7eacd94dcd52bf88e744ecfcb31e7909b2952258
MoedCode <mohamed71291@gmail.com>
2023-10-23 23:44:52 +0300

    Sorting Algorithms (automated command)

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -18,13 +18,6 @@ void swap(int *x, int *y)
 	*x = *y;
 	*y = tmp;
 }
-/**
- * shell_sort - sorts an array of integers in ascending order using the Shell sort algorithm
- * using the Knuth sequence
- * @array: pointer to array that will be sort
- * @size: size of the array
- */
- #include "sort.h"
 
 /**
  * array_reverse - Reverse the order of elements in an integer array.
@@ -36,18 +29,27 @@ void swap(int *x, int *y)
  */
 void array_reverse(int array[], size_t size)
 {
-    size_t start = 0;
-    size_t end = size - 1;
+	size_t start = 0;
+	size_t end = size - 1;
 
-    while (start < end)
-    {
-        int temp = array[start];
-        array[start] = array[end];
-        array[end] = temp;
-        start++;
-        end--;
-    }
+	while (start < end)
+	{
+		int temp = array[start];
+
+		array[start] = array[end];
+		array[end] = temp;
+		start++;
+		end--;
+	}
 }
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the
+ * Shell sort algorithm using the Knuth sequence
+ * @array: pointer to array that will be sort
+ * @size: size of the array
+ */
+ #include "sort.h"
 
 void shell_sort(int *array, size_t size)
 {
@@ -56,21 +58,21 @@ void shell_sort(int *array, size_t size)
 	if (!array || !size)
 		return;
 
-		 gap = size / 2;
+	gap = size / 2;
 	for (pass_count = 1; is_swap; pass_count++)
+	{
+		is_swap = 0;
+		j = gap / pass_count;
+		for (i = 0; j < size; i++, j++)
 		{
-			is_swap = 0;
-			j = gap / pass_count;
-			for(i = 0; j < size; i++, j++)
+			if (array[i] < array[j])
 			{
-				if (array[i] < array[j])
-				{
-					swap(&array[i], &array[j]);
-					if (j == size - 1 && array[i - gap] < array[i] )
-						swap(&array[i - gap], &array[i]);
-					is_swap = 1;
-				}
+				swap(&array[i], &array[j]);
+				if (j == size - 1 && array[i - gap] < array[i])
+					swap(&array[i - gap], &array[i]);
+				is_swap = 1;
 			}
+		}
 			print_array(array, size);
 		}
 		array_reverse(array, size);
