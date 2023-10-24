8067204d181145e0fe82d7e7ff0b0a8fa53d36fd
MoedCode <mohamed71291@gmail.com>
2023-10-24 06:31:59 +0300

    Sorting Algorithms (automated command)

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 #include <stddef.h>
+#include <stdlib.h>
 
  int getMaxInAr(int *array, size_t size)
 {
@@ -19,12 +20,32 @@
 void counting_sort(int *array, size_t size)
 {
 	int max = getMaxInAr(array, size),
-	count_arr[max], i;
+	*count_arr, i, k = 0;
+	size_t  j;
+	count_arr = malloc(sizeof(int) * max);
+
+
 	for (i = 0; i < max; i++)
-		count_arr[i] = 1;
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (array[j] == i)
+					count_arr[i]++;
+			}
+
+		}
+	for (i = 0; i < max; i++)
+	{
+		if (count_arr[i] != 0)
+		{
+			array[k] = i;
+			k++;
+		}
+	}
+
 	print_array(count_arr, max);
 	printf("maix is [%d]", max);
-
+	free(count_arr);
 
 
 }
