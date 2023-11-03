b8ebda0c7dbf9fea57ccd85f794f56b0fbf6c490
MoedCode <mohamed71291@gmail.com>
2023-11-04 01:42:26 +0200

    Radix-sort A miserable, desperate attempt (automated command)

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -5,53 +5,13 @@
 #include <string.h>
 #include <limits.h>
 
-int getMaxInAr(int *array, size_t size);
- int number_len(int n);
-char *int_to_string(int n, int max_len);
-size_t freeDoubleArray(char **argv);
-int count_sort( int array[], size_t size, int pass);
-
-
-void radix_sort(int *array, size_t size)
-{
-	int max_n, pass;
-
-	max_n = getMaxInAr(array, size);
-	for (pass = 1; max_n /  pass  ; pass *= 10)
-	{
-		count_sort(array, size, pass);
-		print_array(array, size);
-	}
-
-}
-
-
 /**
- * number_len - function that gets the length of a given numbers
+ * getMaxInAr - Get the maximum value in an integer array.
+ * @array: Pointer to the array.
+ * @size: Size of the array.
  *
- * parameters:
- * @n: number that it's length wanted to be calculated
- *
- * Return: the given number length
+ * Return: The maximum value in the array, or -1 on error.
  */
-
- int number_len(int n)
-{
-	unsigned int len, divisor;
-
-	for(len = 0, divisor = 10; n; len++)
-			n = n / divisor;
-	return (len);
-}
-/**
- * getMaxInAr - Get the Max value in the Array
- *
- * @array: pointer to array
- * @size: size of array
- * Return: positive (int) which is max, (-1) on error
- */
-
-
 int getMaxInAr(int *array, size_t size)
 {
 	size_t i;
@@ -67,39 +27,93 @@ int getMaxInAr(int *array, size_t size)
 	}
 	return (max);
 }
+
+/**
+ * number_len - Calculate the length of an integer.
+ * @n: The integer for which the length is calculated.
+ *
+ * Return: The length of the integer.
+ */
+int number_len(int n)
+{
+	unsigned int len, divisor;
+
+	for (len = 0, divisor = 10; n; len++)
+		n = n / divisor;
+	return (len);
+}
+
+/**
+ * count_sort - Sort an integer array using counting sort for a specific pass.
+ * @array: The array to be sorted.
+ * @size: Size of the array.
+ * @pass: The current pass (1, 10, 100, etc.).
+ *
+ * Return: 0 on success, -1 on error.
+ */
+int count_sort(int array[], size_t size, int pass);
 int count_sort(int array[], size_t size, int pass)
 {
-    int size_i = (int)size;
-    int *count = calloc(sizeof(int), 10);
-    int *tmp = calloc(sizeof(int), size_i);
-    int i;
+	int size_i = (int)size;
+	int *count = malloc(sizeof(int) * 10);
+	int *tmp = malloc(sizeof(int) * size_i);
+	int i, j;
 
-    if (!pass || !array || !size)
-        return (-1);
+	if (!pass || !array || !size)
+	{
+		free(count);
+		free(tmp);
+		return (-1);
+	}
+	for (j = 0; j < 10; j++)
+		count[j] = 0;
+	for (j = 0; j < size_i; j++)
+		tmp[j] = 0;
 
-    /*Initialize count array with zeros.*/
-    for (i = 0; i < size_i; i++) {
-        ++count[(array[i] / pass) % 10];
-    }
+	/* Initialize count array with zeros. */
+	for (i = 0; i < size_i; i++)
+	{
+		++count[(array[i] / pass) % 10];
+	}
 
-    /*Update count array with prefix sums.*/
-    for (i = 1; i < 10; i++) {
-        count[i] += count[i - 1];
-    }
+	/* Update count array with prefix sums. */
+	for (i = 1; i < 10; i++)
+	{
+		count[i] += count[i - 1];
+	}
 
-    /*Build the output array 'tmp'.*/
-    for (i = size_i - 1; i >= 0; i--) {
-        tmp[--count[(array[i] / pass) % 10]] = array[i];
-    }
+	/* Build the output array 'tmp'. */
+	for (i = size_i - 1; i >= 0; i--)
+	{
+		tmp[--count[(array[i] / pass) % 10]] = array[i];
+	}
 
-    /*Copy 'tmp' back to 'array'.*/
-    for (i = 0; i < size_i; i++) {
-        array[i] = tmp[i];
-    }
+	/* Copy 'tmp' back to 'array'. */
+	for (i = 0; i < size_i; i++)
+	{
+		array[i] = tmp[i];
+	}
 
-    free(count);
-    free(tmp);
-    putchar(10);
+	free(count);
+	free(tmp);
+	putchar(10);
 
-    return (0);
+	return (0);
+}
+
+/**
+ * radix_sort - Sort an integer array using radix sort.
+ * @array: The array to be sorted.
+ * @size: Size of the array.
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max_n, pass;
+
+	max_n = getMaxInAr(array, size);
+	for (pass = 1; max_n / pass; pass *= 10)
+	{
+		count_sort(array, size, pass);
+		print_array(array, size);
+	}
 }
